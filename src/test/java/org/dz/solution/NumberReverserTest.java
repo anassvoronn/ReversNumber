bb17810db420f82ecd646c7d7ef588b1ad0ec223
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-13 13:13:04 +0300

    NumberReverser

--- src/test/java/org/dz/solution/NumberReverserTest.java
+++ src/test/java/org/dz/solution/NumberReverserTest.java
@@ -3,6 +3,7 @@ package org.dz.solution;
 import org.junit.Test;
 
 public class NumberReverserTest {
+
     @Test
     public void reversAndPrintFor1234() {
         NumberReverser reverser = new NumberReverser();
@@ -18,6 +19,12 @@ public class NumberReverserTest {
     @Test
     public void reversAndPrintFor1() {
         NumberReverser reverser = new NumberReverser();
+        reverser.reversAndPrint(1);
+    }
+
+    @Test
+    public void reversAndPrintFor2() {
+        NumberReverser reverser = new NumberReverser();
         reverser.reversAndPrint(2);
     }
 
@@ -29,6 +36,30 @@ public class NumberReverserTest {
 
     @Test
     public void reversAndPrintFor10() {
+        NumberReverser reverser = new NumberReverser();
+        reverser.reversAndPrint(10);
+    }
+
+    @Test
+    public void reversAndPrintFor100() {
+        NumberReverser reverser = new NumberReverser();
+        reverser.reversAndPrint(100);
+    }
+
+    @Test
+    public void reversAndPrintFor1200() {
+        NumberReverser reverser = new NumberReverser();
+        reverser.reversAndPrint(1200);
+    }
+
+    @Test
+    public void reversAndPrintFor200() {
+        NumberReverser reverser = new NumberReverser();
+        reverser.reversAndPrint(200);
+    }
+
+    @Test
+    public void reversAndPrintFor11() {
         NumberReverser reverser = new NumberReverser();
         reverser.reversAndPrint(11);
     }
