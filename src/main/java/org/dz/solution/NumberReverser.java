b95f8cb7fc9fe367122aaaa688d3ea55b337fdce
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-16 14:13:36 +0300

    NumberReverser

--- src/main/java/org/dz/solution/NumberReverser.java
+++ src/main/java/org/dz/solution/NumberReverser.java
@@ -2,9 +2,12 @@ package org.dz.solution;
 
 public class NumberReverser {
     public void reversAndPrint(final int number) {
+        if (number == 0){
+            System.out.print(0);
+        }
         int result;
         int delimiter = 1;
-        while (delimiter < number) {
+        while (delimiter <= number) {
             delimiter = delimiter * 10;
             result = number % delimiter;
             result = result / (delimiter / 10);
